f565e59d21c435c097cbc8224f7327ec1f752f3f
Akinola Abiodun <akinolaemmanuel49@gmail.com>
2024-05-11 17:44:36 +0100

    implement landing page skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,33 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import MemoLogo from './ui/memo-logo';
 
 export default function LandingPage() {
   return (
-    <div className="flex flex-row max-h-screen md:flex-col">
-      <div className="flex h-screen">
-        <div className="relative flex w-1/2 max-h-full">
-          <Image
-            src="https://th.bing.com/th/id/R.4844312b1079ee0b6bd1cd8f90bfbb60?rik=ApGEZ%2fi7yZDeLA&riu=http%3a%2f%2fwww.pixelstalk.net%2fwp-content%2fuploads%2f2016%2f08%2fSuper-cool-picture-backgrounds.jpg&ehk=sgEHfm4fVk4HT36alHTZyd8CFpN6JO%2bToNd%2berN5HZE%3d&risl=&pid=ImgRaw&r=0"
-            layout="fill"
-            objectFit="cover"
-            alt="Cool picture"
-            className="">
-          </Image>
+    <div className="flex flex-col h-screen md:flex-row md:w-screen">
+      <div className="relative w-full h-32 md:w-1/4 sm:h-1/2-sm">
+        <div className="flex flex-col justify-center items-center h-32 md:justify-center md:items-end bg-black md:h-screen">
+          <MemoLogo />
         </div>
-        <div className="flex px-4 mt-12 justify-start items-center">
-          <main>
-            <h1 className="text-black text-wrap text-[50px] font-bold">Welcome to Memo</h1>
-            <p className="text-black text-wrap text-[25px] font-bold">Join today.</p>
-            <Link href="/signup">
-              Signup
-            </Link>
-            Already have an account?
-            <Link href="/signin">
-              Signin
-            </Link>
-          </main>
-        </div>
+      </div>
+      <div className="flex flex-row px-4 mt-12 justify-center items-center">
+        <main>
+          <h1 className="text-black text-wrap text-[50px] font-bold p-4">Welcome to Memo</h1>
+          <p className="text-black text-wrap text-[25px] font-bold p-4">Join today.</p>
+          <div className="flex flex-col w-full items-center">
+            <div>
+              <Link href="/signup" className="flex flex-col h-[48px] w-64 grow items-center justify-center gap-2 rounded-lg bg-blue-500 p-3 text-md text-white font-medium hover:bg-gray-600 hover:text-white outline-blue-500 md:flex-none md:justify-start md:p-2 md:px-3">
+                Create account
+              </Link>
+            </div>
+            <p className="p-6">Already have an account?</p>
+            <div>
+              <Link href="/signin" className="flex flex-col h-[48px] w-64 grow items-center justify-center gap-2 rounded-lg bg-white p-3 text-md font-medium hover:bg-gray-600 hover:text-white outline-gray-600 md:flex-none md:justify-start md:p-2 md:px-3">
+                Sign in
+              </Link>
+            </div>
+          </div>
+        </main>
       </div>
     </div>
   )
